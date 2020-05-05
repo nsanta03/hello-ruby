d8126231f35fdf1dcd6ad919d2ad34c7e29e44a9
Brian Eng <brian@brianeng.com>
2020-05-05 14:14:01 -0500

    initial commit

---
+++ exercises/5.rb
@@ -0,0 +1,15 @@
+# To run this code, be sure your current working directory
+# is the same as where this file is located.
+# ruby 5.rb
+
+# EXERCISE
+# Build a deck of cards. Given the following arrays, use a loop
+# (or two... hint, hint) to write out every combination to the
+# screen.
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+# BONUS
+# Build the deck so that it doesn't just write the combinations out;
+# rather, create a new data structure that holds the cards so they 
+# can be shuffled, dealt, etc.
